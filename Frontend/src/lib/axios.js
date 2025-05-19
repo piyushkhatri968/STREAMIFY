34f74e7c62bd6ba0abfad25d24010b1dde7b3c10
Piyush Khatri <piyushkhatri968@gmail.com>
2025-05-20 04:41:17 +0500

    .

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://streamify-sigma-six.vercel.app/api",
+  baseURL: "https://streamify-backend-ebon.vercel.app/api",
   withCredentials: true,
 });
