cd7efb38ad98f7f50e57122dd5b80774d7585c7a
Piyush Khatri <piyushkhatri968@gmail.com>
2025-05-22 01:57:26 +0500

    .

--- Frontend/src/pages/NotificationsPage.jsx
+++ Frontend/src/pages/NotificationsPage.jsx
@@ -26,7 +26,7 @@ const NotificationsPage = () => {
   const incomingRequests = friendRequests?.incomingReqs || [];
   const acceptedRequests = friendRequests?.acceptedReqs || [];
   return (
-    <div className="p-4 sm:p-6 lg:p-8">
+    <div className="p-4 sm:p-6 lg:p-8 min-h-screen">
       <div className="contaienr mx-auto max-w-4xl space-y-8">
         <h1 className="text-2xl sm:text-3xl font-bold tracking-tight mb-6">
           Notifications
