e9a12edc71c66dd9a3be8ce7297fcce02688954a
Piyush Khatri <piyushkhatri968@gmail.com>
2025-05-22 01:59:30 +0500

    .

--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -1,18 +1,20 @@
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { useLocation } from "react-router";
 
 const Layout = ({ children, showSidebar = false }) => {
+  const location = useLocation();
   return (
-    <div className="min-h-screen flex flex-col">
-      <div className="flex flex-1">
+    <div className="min-h-screen">
+      <div className="flex">
         {showSidebar && <Sidebar />}
         <div className="flex-1 flex flex-col">
           <Navbar />
           <main className="flex-1 overflow-y-auto">{children}</main>
         </div>
       </div>
-      <Footer />
+      {location.pathname !== "/chat" && <Footer />}
     </div>
   );
 };
